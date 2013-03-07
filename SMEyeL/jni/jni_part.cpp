15c9d4ca386537682bc1750a99d1ec2ece1dc8e4
scorpeeon <scorpeeon@gmail.com>
2013-03-08 00:55:36 +0100

    SMEyeL initial commit

---
+++ SMEyeL/jni/jni_part.cpp
@@ -0,0 +1,51 @@
+#include <jni.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+extern "C" {
+JNIEXPORT void JNICALL Java_com_aut_smeyel_MainActivity_FindFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba);
+
+JNIEXPORT void JNICALL Java_com_aut_smeyel_MainActivity_FindFeatures(JNIEnv*, jobject, jlong addrGray, jlong addrRgba)
+{
+    Mat& mGr  = *(Mat*)addrGray;
+    Mat& mRgb = *(Mat*)addrRgba;
+    vector<KeyPoint> v;
+
+    FastFeatureDetector detector(50);
+    detector.detect(mGr, v);
+    for( unsigned int i = 0; i < v.size(); i++ )
+    {
+        const KeyPoint& kp = v[i];
+        circle(mRgb, Point(kp.pt.x, kp.pt.y), 10, Scalar(255,0,0,255));
+    }
+}
+
+JNIEXPORT void JNICALL Java_com_aut_smeyel_MainActivity_FindCircles(JNIEnv*, jobject, jlong addrGray, jlong addrRgba);
+
+JNIEXPORT void JNICALL Java_com_aut_smeyel_MainActivity_FindCircles(JNIEnv*, jobject, jlong addrGray, jlong addrRgba)
+{
+    Mat& mGr  = *(Mat*)addrGray;
+    Mat& mRgb = *(Mat*)addrRgba;
+
+    // Reduce the noise so we avoid false circle detection
+    GaussianBlur( mGr, mGr, Size(9, 9), 2, 2 );
+
+    vector<Vec3f> circles;
+    HoughCircles(mGr, circles, CV_HOUGH_GRADIENT, 2, mGr.rows/4, 200, 100 );
+
+    for( int i = 0; i < circles.size(); i++ )
+    {
+    	Point center(cvRound((double)circles[i][0]), cvRound((double)circles[i][1]));
+    	int radius = cvRound((double)circles[i][2]);
+    	// draw the circle center
+    	circle( mRgb, center, 3, Scalar(0,255,0), -1, 8, 0 );
+    	// draw the circle outline
+    	circle( mRgb, center, radius, Scalar(0,0,255), 3, 8, 0 );
+    }
+}
+}
