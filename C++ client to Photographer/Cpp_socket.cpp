75028b11eb224119799e159294ce3e466d41d2bb
Kristof <kristof@aut.bme.hu>
2013-03-26 11:25:38 +0100

    DummySmartphone created and testing environment with it.

--- C++ client to Photographer/Cpp_socket.cpp
+++ C++ client to Photographer/Cpp_socket.cpp
@@ -24,19 +24,132 @@ static void error_exit(char *errorMessage) {
     exit(EXIT_FAILURE);
 }
 
-int main( int argc, char *argv[]) {
-    struct sockaddr_in server;
-    struct hostent *host_info;
-    unsigned long addr;
-	char *ip_buff = "192.168.74.103"; //ip beállítása
-	int iResult;
-    SOCKET sock;
+void SendPingRequest(int conn)
+{
     char *echo_string;
     int echo_len;
-
-	echo_string = "p"; // p-betû -->képkészítés; ping --> pingelés
+	echo_string = "{ \"type\": \"ping\" }";
     echo_len = strlen(echo_string);
 
+    if (send(conn, echo_string, echo_len, 0) != echo_len)
+        error_exit("send() has sent a different number of bytes than expected !!!!");
+}
+
+void SendTakePhotoRequest(int conn, int DesiredTimeStamp)
+{
+    char buffer[100];
+    int len;
+	sprintf(buffer,"{ \"type\": \"takepicture\", \"desiredtimestamp\": \"%d\" }",DesiredTimeStamp);
+    len = strlen(buffer);
+
+    if (send(conn, buffer, len, 0) != len)
+        error_exit("send() has sent a different number of bytes than expected !!!!");
+}
+
+void ProcessIncomingJSON(int sock,char *buffer)
+{
+	cout << "JSON received:" << endl << buffer << endl << "End of JSON" << endl;
+
+	// Parse JSON
+	char *posJPEG = strstr(buffer,"JPEG");
+	char *posTimeStamp = strstr(buffer,"timestamp");
+	char *posPong = strstr(buffer,"pong");
+
+	if (posPong)
+	{
+		cout << "PONG received, nothing left to do..." << endl;
+	}
+	else if (posJPEG)
+	{
+		char tmpS[100];
+		char *posTimeStampValue = posTimeStamp + 13;
+		char *posTimeStampValueEnd = strstr(posTimeStampValue,"\"");
+		int len = posTimeStampValueEnd-posTimeStampValue;
+		strncpy(tmpS, posTimeStampValue, len );
+		*(tmpS+len) = 0;
+		int timestamp = atoi(tmpS);
+
+		char *posSize = strstr(buffer,"size");
+		char *posSizeValue = posSize + 8;
+		char *posSizeValueEnd = strstr(posSizeValue,"\"");
+		len = posSizeValueEnd-posSizeValue;
+		strncpy(tmpS, posSizeValue, len );
+		*(tmpS+len) = 0;
+		int jpegSize = atoi(tmpS);
+
+		cout << "Receiving JPEG. Timestamp=" << timestamp << ", size=" << jpegSize << endl;
+	
+		char receiveBuffer[RCVBUFSIZE];
+		int received;
+		int jpegBytes = 0;
+
+		ofstream outFile;
+		if (outFile != NULL) 
+		{
+			outFile.open("D:\\test.jpg" , ofstream::binary);
+			cout << "File opened!" << endl;
+		} else 
+		{
+			cout << "Can't open file!" << endl;
+		}
+
+		while ((received = recv(sock, receiveBuffer, RCVBUFSIZE, 0)) > 0) 
+		{
+			jpegBytes += received;
+			if (outFile.is_open()) 
+			{
+				outFile.write(receiveBuffer, received); 
+				cout << " (Total: " << jpegBytes << " B)" << endl;
+			}
+			else
+			{
+				cout << "Error in recv() function, received bytes = " << received << endl;
+			}
+		}
+
+		outFile.flush();
+		outFile.close();
+
+		cout << "JPEG received successfully. Sent size=" << jpegSize << ", received size=" << jpegBytes << endl;
+	}
+}
+
+// ----------------------------------------------------------------------------
+/*
+class PhoneProxy
+{
+public:
+
+	void RequestPhoto(int desiredTimeStamp, char* filename);
+	void RequestPing();
+
+private:
+	SOCKET sock;
+	void Connect(char *ip, int port);
+	void Disconnect();
+}
+
+void PhoneProxy::RequestPhoto(int desiredTimeStamp, char* filename)
+{
+}
+
+void PhoneProxy::RequestPing()
+{
+}
+
+void PhoneProxy::Connect(char *ip, int port)
+{
+	struct sockaddr_in server;
+    struct hostent *host_info;
+    unsigned long addr;
+	char *ip_buff = "127.0.0.1"; //ip beállítása
+	int iResult;
+
+	char tmpBuff[100];
+	cout << "Press enter to start connecting..." << endl;
+	cin >> tmpBuff;
+
+	cout << "Connecting..." << endl;
 
     WORD wVersionRequested;
     WSADATA wsaData;
@@ -46,9 +159,61 @@ int main( int argc, char *argv[]) {
     else
         printf("Winsock Initialised\n");
 
-
     sock = socket( AF_INET, SOCK_STREAM, 0 );
 
+    if (sock < 0)
+        error_exit( "Socket error");
+
+    memset( &server, 0, sizeof (server));
+    if ((addr = inet_addr( ip_buff)) != INADDR_NONE) {
+        memcpy( (char *)&server.sin_addr, &addr, sizeof(addr));
+    }
+    else {
+        host_info = gethostbyname(ip_buff);
+        if (NULL == host_info)
+            error_exit("Unknown Server");
+        memcpy( (char *)&server.sin_addr,
+                host_info->h_addr, host_info->h_length );
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons( PORT );
+
+    if(connect(sock,(struct sockaddr*)&server,sizeof(server)) <0)
+        error_exit("Connection to the server failed");
+}
+
+void PhoneProxy::Disconnect()
+{
+}
+
+
+*/
+
+int main( int argc, char *argv[])
+{
+    struct sockaddr_in server;
+    struct hostent *host_info;
+    unsigned long addr;
+	char *ip_buff = "127.0.0.1"; //ip beállítása
+	int iResult;
+    SOCKET sock;
+
+	char tmpBuff[100];
+	cout << "Press enter to start connecting..." << endl;
+	cin >> tmpBuff;
+
+	cout << "Connecting..." << endl;
+
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    wVersionRequested = MAKEWORD (1, 1);
+    if (WSAStartup (wVersionRequested, &wsaData) != 0)
+        error_exit( "Initialisation of Winsock failed");
+    else
+        printf("Winsock Initialised\n");
+
+    sock = socket( AF_INET, SOCK_STREAM, 0 );
 
     if (sock < 0)
         error_exit( "Socket error");
@@ -71,8 +236,8 @@ int main( int argc, char *argv[]) {
     if(connect(sock,(struct sockaddr*)&server,sizeof(server)) <0)
         error_exit("Connection to the server failed");
 
-    if (send(sock, echo_string, echo_len, 0) != echo_len)
-        error_exit("send() has sent a different number of bytes than expected !!!!");
+	SendPingRequest(sock);
+	//SendTakePhotoRequest(sock, 123456);
 
 	iResult = shutdown(sock, SD_SEND);
 
@@ -83,54 +248,37 @@ int main( int argc, char *argv[]) {
         return 1;
     }
 
-
-	//image reception
+	// Receive response
 	int totalBytes = 0;
-	ofstream outFile;
-	char buf[1024] = "";
+	char buffer[RCVBUFSIZE] = "";
 	int received = 0;
-	char pong_buff[5];
 
-	if(echo_string == "p")
+	// Read JSON part (1 byte at once)
+	char c;
+	char *bufPtr = buffer;
+	*bufPtr = 0;
+	bool processed = false;
+	while ((received = recv(sock, &c, 1, 0)) > 0) 
 	{
-		if (outFile != NULL) 
+		if (c != '\0')	// Not at end of JSON
 		{
-			outFile.open("D:\\test.jpg" , ofstream::binary);
-			cout << "File opened!" << endl;
-		} else 
-		{
-			cout << "Can't open file!" << endl;
+			*bufPtr = c;
+			bufPtr++;
+			*bufPtr = 0;
 		}
-
-		while ((received = recv(sock, buf, sizeof(buf), 0)) > 0) 
+		else
 		{
-			cout << "R:" << received << " ";
-			if (received > 0) 
-			{
-				totalBytes += received;
-				if (outFile.is_open()) 
-				{
-					outFile.write(buf, received); 
-					cout << " (Total: " << totalBytes << " B)" << endl;
-				} else
-				cout << "Error in recv() function, received bytes = " << received << endl;
-			} else 
-				cout << "R:" << received << " ";
-		}	
-		outFile.close();
+			// End of JSON
+			ProcessIncomingJSON(sock,buffer);
+			processed = true;
+		}
+	}
+	if (!processed)
+	{
+		ProcessIncomingJSON(sock,buffer);
 	}
 
-	if(echo_string == "ping")
-		{
-			recv(sock, buf, sizeof(buf),0);
-			for(int i=2; i<6; i++)
-			{
-				pong_buff[i-2]=buf[i];
-			}
-			pong_buff[4]='\0';
-			cout<<pong_buff<< endl;
-		}
-		closesocket(sock);
-		WSACleanup();
-		return EXIT_SUCCESS;
+	closesocket(sock);
+	WSACleanup();
+	return EXIT_SUCCESS;
 }
