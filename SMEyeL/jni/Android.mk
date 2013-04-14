e6895920d6c5bc9a077139c117063b6309f33fa4
scorpeeon <scorpeeon@gmail.com>
2013-04-15 01:42:19 +0200

    SMEyeL updated (use wildcards in Android.mk)

--- SMEyeL/jni/Android.mk
+++ SMEyeL/jni/Android.mk
@@ -5,17 +5,11 @@ include $(CLEAR_VARS)
 include ../../../android-opencv/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := native_sample
-LOCAL_SRC_FILES := jni_part.cpp \
-                   libMiscTimeAndConfig/src/ConfigManagerBase.cpp \
-                   libMiscTimeAndConfig/src/TimeMeasurement.cpp \
-                   libTwoColorCircleMarker/src/FastColorFilter.cpp \
-                   libTwoColorCircleMarker/src/MarkerCC2.cpp \
-                   libTwoColorCircleMarker/src/MarkerCC2Locator.cpp \
-                   libTwoColorCircleMarker/src/MarkerCC2Tracker.cpp \
-                   libTwoColorCircleMarker/src/TwoColorLocator.cpp \
-                   miscLogConfig/src/ConfigReader.cpp \
-                   miscLogConfig/src/Logger.cpp \
-                   miscLogConfig/src/AndroidLogger.cpp
+FILE_LIST := $(wildcard $(LOCAL_PATH)/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/libMiscTimeAndConfig/src/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/libTwoColorCircleMarker/src/*.cpp)
+FILE_LIST += $(wildcard $(LOCAL_PATH)/miscLogConfig/src/*.cpp)
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 LOCAL_LDLIBS +=  -llog -ldl
 
 include $(BUILD_SHARED_LIBRARY)
